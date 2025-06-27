dc5f698ab7b9bd9c69ad3237a0460315c8632865
The-King-12345 <alexseidinger15@gmail.com>
2025-06-27 11:20:47 -0700

    2024 day01

---
+++ 2024/day01/main.py
@@ -0,0 +1,40 @@
+def read_input(input_file_dir: str) -> ([int], [int]):
+    with open(input_file_dir) as f:
+        lines = f.read().splitlines()
+    
+    list1 = []
+    list2 = []
+    for line in lines:
+        num1, num2 = line.split()
+        list1.append(int(num1))
+        list2.append(int(num2))
+    return list1, list2
+
+def calc_distance(list1: [int], list2: [int]) -> int:
+    list1 = sorted(list1)
+    list2 = sorted(list2)
+
+    sum = 0
+    for i in range(len(list1)):
+        sum += abs(list1[i] - list2[i])
+    return sum
+
+def calc_similarity(list1: [int], list2: [int]) -> int:
+    freq = {}
+    for num in list2:
+        if num in freq:
+            freq[num] += 1
+        else:
+            freq[num] = 1
+    
+    sum = 0
+    for num in list1:
+        if num in freq:
+            sum += num * freq[num]
+    return sum
+
+if __name__ == "__main__":
+    list1, list2 = read_input("input.txt")
+    print(calc_distance(list1, list2))
+    print(calc_similarity(list1, list2))
+
